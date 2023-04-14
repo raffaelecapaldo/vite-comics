d62bcb0a64c3ef6cea0192d7615f504992c68385
raffaelecapaldo <raffaelecpl@gmail.com>
2023-04-14 15:09:42 +0200

    Added extra style from the mockup

--- src/components/ComicCards.vue
+++ src/components/ComicCards.vue
@@ -1,8 +1,15 @@
 <template>
     <div class="comics">
+     
   <div class="container">
+    <div class="showtitle text-white fs-3 fw-bold text-uppercase">
+            current series 
+        </div>
     <div class="row pt-5">
   <ComicCard v-for="comic in comics" :image="comic.thumb" :title="comic.series" />
+</div>
+<div class="load text-white d-flex justify-content-center text-uppercase">
+    <span>load more</span>
 </div>
 </div>
  </div>
@@ -27,7 +34,25 @@ import ComicCard from './ComicCard.vue';
 
 <style lang="scss" scoped>
 .comics {
-
 background-color: #1c1c1c;
+.container {
+    span {
+        background-color: #0282f9;
+        padding:10px 50px;
+        margin-top:35px;
+        margin-bottom:20px;
+        font-weight: bold;
+        cursor:pointer;
+    }
+    position:relative
+
+}
+.showtitle {
+    position: absolute;
+    top:-40px;
+    left:-5px;
+    background-color: #0282f9;
+    padding:10px 18px;
+}
 }
 </style>
