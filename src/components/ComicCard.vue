75b9c7068b277b8e23819f32a84c1b529363373f
raffaelecapaldo <raffaelecpl@gmail.com>
2023-04-14 14:04:58 +0200

    Maded Card responsive

--- src/components/ComicCard.vue
+++ src/components/ComicCard.vue
@@ -1,6 +1,6 @@
 <template>
- <div class="col-2">
-    <div class="comic-card mt-3 me-3">
+ <div class="col-xxl-2 col5 col-lg-3 col-md-4 col-sm-6 col-12  d-flex d-sm-block justify-content-center align-items-center">
+    <div class="comic-card mt-3">
         <img class="cover" :src="image" :alt="title">
         <p class="text-white text-uppercase mt-3">{{ title }}</p>
     </div>
@@ -25,4 +25,9 @@
     width: 187px;
     height: 187px;
 }
+
+@media (min-width: 1200px) {.col5 {
+    flex: 0 0 auto;
+    width: 20%;
+}}
 </style>
